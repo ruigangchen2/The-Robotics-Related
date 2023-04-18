330027fcc96ae699b68ff2f4eb75745e5ddd25f3
ruigangchen2 <ruigang_chen2@163.com>
2023-04-18 21:58:20 +0800

    update the name

--- Robotics_Arm_Orangepi5/Robotics_Arm_Orangepi5.c
+++ Robotics_Arm_Orangepi5/Robotics_Arm_Orangepi5.c
@@ -12,27 +12,28 @@
 Reference:
 https://zhou-yuxin.github.io/articles/2017/Linux的GPIO子系统之-sys-class-gpio目录/index.html
 
-gcc -o robotic_armV2 robotic_armV2.c -lwiringPi -pthread -lcrypt -lm -lrt
+gcc -o Robotics_Arm_Orangepi5 Robotics_Arm_Orangepi5.c -lwiringPi -pthread -lcrypt -lm -lrt
 
-echo 92 > /sys/class/gpio/export
-echo 52 > /sys/class/gpio/export
+
+echo 92 > /sys/class/gpio/export  # Encoder A Phase 
 echo in > /sys/class/gpio/gpio92/direction
-echo in > /sys/class/gpio/gpio52/direction
 echo falling > /sys/class/gpio/gpio92/edge
 */
 
 #define encoderB_pin 15
 #define electromagnet_1 9
-#define electromagnet_2 10
+#define electromagnet_2 6
+#define electromagnet_3 4
+#define electromagnet_4 3
 
 /***** AB encoder variable *****/
-float pulse = 0; //记录A脉冲的次敄1�7
-float motion = 0; //定义位移
+float pulse = 0;  //count the pulse of the encoder
+float motion = 0; 
 char forward_direction = '+';
 char back_direction = '-';
-char current_direction = 0; //方向
+char current_direction = 0; 
 char direction_state = 0;
-float velocity=0; //速度
+float velocity=0; 
 /******************************/
 
 /***** Testing time *****/
@@ -43,20 +44,19 @@ struct timeval EndTime;
 
 struct pollfd fds[1];
 
-void get_info()  //得出角度
+void get_info()  //calculate the information of the encoder
 {
-    velocity = pulse * 60.0; //   n / (5 * 0.001 * 1200) * 360
+    velocity = pulse * 48.0; //   n / (5 * 0.001 * 1500) * 360
 
     if(current_direction == forward_direction){
-        motion = motion + pulse * 360 / 1200; //计算正位秄1�7
+        motion = motion + pulse * 360 / 1500; //calculate the positive motion 
     }
     else{
-        motion = motion - pulse * 360 / 1200; //计算负位秄1�7
+        motion = motion - pulse * 360 / 1500; //calculate the negative motion 
         velocity = -velocity;
     }
-    pulse = 0;    //脉冲A计数彄1�70
+    pulse = 0;    //set pulse to zero
     printf("motion is:%.2f\n",motion);
-//   printf("speed is:%.2f\n",velocity);
 }
 
 
@@ -87,7 +87,7 @@ void *create(void)
                 return (void *)-1;
             }
             else{
-                if(digitalRead(encoderB_pin) == HIGH){  //B脉冲为高电平
+                if(digitalRead(encoderB_pin) == HIGH){  //if B pin is in the high place
                     current_direction = forward_direction;
                 }
                 else{
@@ -101,14 +101,14 @@ void *create(void)
 
 void testingT_start()
 {
-    gettimeofday(&StartTime, NULL);  //测量弢�姄1�7
+    gettimeofday(&StartTime, NULL);  //measure the time
 }
 
 double testingT_end()
 {
-    gettimeofday(&EndTime, NULL);   //测量结束
+    gettimeofday(&EndTime, NULL);   //measurement ends
     TimeUse = 1000000*(EndTime.tv_sec-StartTime.tv_sec)+EndTime.tv_usec-StartTime.tv_usec;
-    TimeUse/=1000;  //测量结果，毫秒级
+    TimeUse/=1000;  //the result is in the ms dimension
     return TimeUse;
 }
 
@@ -117,6 +117,8 @@ static volatile int file_state = 1;
 void intHandler(int i){
     digitalWrite(electromagnet_1,0);
     digitalWrite(electromagnet_2,0);
+    digitalWrite(electromagnet_3,0);
+    digitalWrite(electromagnet_4,0);
     file_state = 0;
 }
 
@@ -124,31 +126,26 @@ void *Timer_5ms(void)
 {   
     testingT_start();
 
-    wiringPiSetup();
-    pinMode(encoderB_pin, INPUT);
-    
-    pinMode(electromagnet_1, OUTPUT);
-    pinMode(electromagnet_2, OUTPUT);
-    
     digitalWrite(electromagnet_1,0);
     digitalWrite(electromagnet_2,1);
-    int Upper_Clutch = 0;
-    int Nether_Clutch = 1;
 
-    FILE *fp = fopen("/root/robotic_arm/robotic_arm.csv", "w+");
+    int Electromagnet_1_Clutch = 0;
+    int Electromagnet_2_Clutch = 1;
+
+    FILE *fp = fopen("./robotic_arm.csv", "w+");
     if (fp == NULL) {
         fprintf(stderr, "fopen() failed.\n");
         printf("Failed\n");
         exit(EXIT_FAILURE);
     }
-    fprintf(fp, "Time,Degree,Velocity,Upper_Clutch,Nether_Clutch\n");
+    fprintf(fp, "Time,Degree,Velocity,Electromagnet_1_Clutch,Electromagnet_2_Clutch\n");
     
     signal(SIGINT, intHandler);
     while(1){
         get_info();
         
         if(file_state == 1)
-            fprintf(fp,"%.2f,%.2f,%.2f,%d,%d\n",testingT_end(),motion,velocity,Upper_Clutch,Nether_Clutch);
+            fprintf(fp,"%.2f,%.2f,%.2f,%d,%d\n",testingT_end(),motion,velocity,Electromagnet_1_Clutch,Electromagnet_2_Clutch);
         else
             fclose(fp); 
         if(motion > 45)
@@ -156,8 +153,8 @@ void *Timer_5ms(void)
         if(motion <= 0.3 && file_state == 1 && direction_state == forward_direction){
             digitalWrite(electromagnet_1,1);
             digitalWrite(electromagnet_2,0);
-            Upper_Clutch = 1;
-            Nether_Clutch = 0;
+            Electromagnet_1_Clutch = 1;
+            Electromagnet_2_Clutch = 0;
         }
         delay(5);
     }
@@ -167,6 +164,20 @@ void *Timer_5ms(void)
 
 int main(int argc, const char *argv[])
 {
+    wiringPiSetup();
+    pinMode(encoderB_pin, INPUT);
+    
+    pinMode(electromagnet_1, OUTPUT);
+    pinMode(electromagnet_2, OUTPUT);
+    pinMode(electromagnet_3, OUTPUT);
+    pinMode(electromagnet_4, OUTPUT);
+    
+    digitalWrite(electromagnet_1,0);
+    digitalWrite(electromagnet_2,0);
+    digitalWrite(electromagnet_3,0);
+    digitalWrite(electromagnet_4,0);
+    delay(500);
+    
 	pthread_t id1,id2;
 	int value;
 
--- Robotics_Arm_Orangepi5/robotic_arm.py
+++ Robotics_Arm_Orangepi5/robotic_arm.py
@@ -12,10 +12,10 @@ degree = np.array(data['Degree'].ravel())
 degree = np.around(degree,2)
 velocity = np.array(data['Velocity'].ravel())
 velocity = np.around(velocity,2)
-upper_clutch = np.array(data['Upper_Clutch'].ravel())
-upper_clutch = np.around(upper_clutch,2)
-nether_clutch = np.array(data['Nether_Clutch'].ravel())
-nether_clutch = np.around(nether_clutch,2)
+# upper_clutch = np.array(data['Upper_Clutch'].ravel())
+# upper_clutch = np.around(upper_clutch,2)
+# nether_clutch = np.array(data['Nether_Clutch'].ravel())
+# nether_clutch = np.around(nether_clutch,2)
 
 
 
@@ -39,27 +39,27 @@ ax1.set_ylim([-180, 180])
 ax2.set_ylim([-1200, 1200])
 
 fig.savefig('The Angular Displacement & Velocity.pdf')
+plt.show()
 
 
+# fig ,ax1 = plt.subplots(figsize=(8, 4), dpi=100)
+# ax2 = ax1.twinx()
 
-fig ,ax1 = plt.subplots(figsize=(8, 4), dpi=100)
-ax2 = ax1.twinx()
+# ax1.plot(time, upper_clutch, 'b-*', label='Upper Electromagnet State [On/Off]')
+# ax2.plot(time, nether_clutch,'r-*', label='Upper Electromagnet State [On/Off]')
 
-ax1.plot(time, upper_clutch, 'b-*', label='Upper Electromagnet State [On/Off]')
-ax2.plot(time, nether_clutch,'r-*', label='Upper Electromagnet State [On/Off]')
+# ax1.set_xlabel('Time [ms]', fontweight ='bold')
 
-ax1.set_xlabel('Time [ms]', fontweight ='bold')
+# ax1.set_ylabel('Upper Electromagnet State [On/Off]',fontweight ='bold')
+# ax2.set_ylabel('Upper Electromagnet State [On/Off]',fontweight ='bold')
 
-ax1.set_ylabel('Upper Electromagnet State [On/Off]',fontweight ='bold')
-ax2.set_ylabel('Upper Electromagnet State [On/Off]',fontweight ='bold')
+# ax1.grid()
+# fig.legend()
 
-ax1.grid()
-fig.legend()
+# ax1.set_ylim([-2, 2])
+# ax2.set_ylim([-2, 2])
 
-ax1.set_ylim([-2, 2])
-ax2.set_ylim([-2, 2])
-
-fig.savefig('The Electromagnet State.pdf')
+# fig.savefig('The Electromagnet State.pdf')
 
 
 
