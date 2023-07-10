fbcf8afd28df48c5b1194b7a31ad20cb75929e50
CotyChen <ruigang_chen2@163.com>
2023-07-10 08:57:10 +0800

    updated

--- Python-Analysis/pythonProject/Ruigang_Robotic_Arm-Simu_Combination.py
+++ Python-Analysis/pythonProject/Ruigang_Robotic_Arm-Simu_Combination.py
@@ -48,60 +48,44 @@ angle_ = np.append(angle, angle3)
 velocity = np.append(velocity1, velocity2)
 velocity_ = np.append(velocity, velocity3)
 
-def eom1(_t, y):
+
+def eom1(_t, y):  # y =  - k/J * (90 + y0)
     return [y[1], - stiffness1 / inertia * y[0]]
+
 
 def eom2(_t, y):
     return [y[1], - torque_friction1 / inertia]
 
+
 def eom3(_t, y):
     return [y[1], - stiffness2 / inertia * y[0]]
+
 
 sol_ivp1 = solve_ivp(eom1, [0, 0.09], [angle1[0], velocity1[0]], max_step=0.001)
 sol_ivp2 = solve_ivp(eom2, [0, 0.38], [angle2[0], velocity2[0]], max_step=0.001)
 sol_ivp3 = solve_ivp(eom3, [0, 0.15], [angle3[0], velocity3[0]], max_step=0.001)
 
-
-y_list = []
-def y(t):
-    for i in t:
-        if i >= 0 and i <= time1[-1]:
-            y_list = np.append(0, sol_ivp1.y[0] * 180 / np.pi)
-        elif i > time1[-1] and i <= time[-1]:
-            y_list = np.append(y_list, sol_ivp2.y[0] * 180 / np.pi)
-        elif i > time[-1]:
-            y_list = np.append(y_list, sol_ivp3.y[0] * 180 / np.pi)
-y(time_)
-
-order = 15
-z = np.polyfit(time1, velocity1, order)
-p = np.poly1d(z)
-velocity_fit1 = p(time1)
-
-z = np.polyfit(time2, velocity2, order)
-p = np.poly1d(z)
-velocity_fit2 = p(time2)
-
-z = np.polyfit(time3, velocity3, order)
-p = np.poly1d(z)
-velocity_fit3 = p(time3)
-
-plt.figure(figsize=(6, 5), dpi=100)
+plt.figure(figsize=(7, 6), dpi=100)
 plt.subplot(211)
-plt.plot(time_, angle_ * 180 / np.pi, 'b-+', label=r'$\theta_{exp}$')
-plt.plot(time, y_list, 'r-*', label=r"$\theta_{Simu}$")
+plt.plot(time_, angle_ * 180 / np.pi, 'k-*', label=r'$\theta_{exp}$')
+plt.plot(sol_ivp1.t, sol_ivp1.y[0] * 180 / np.pi, 'r-+', label=r"$\theta_{Simu_1}$")
+plt.plot(sol_ivp1.t[-1] + sol_ivp2.t, sol_ivp2.y[0] * 180 / np.pi, 'b-+', label=r"$\theta_{Simu_2}$")
+plt.plot((sol_ivp1.t[-1] + sol_ivp2.t)[-1] + sol_ivp3.t, sol_ivp3.y[0] * 180 / np.pi, 'g-+', label=r"$\theta_{Simu_3}$")
 plt.grid()
 plt.xlabel('Time [s]')
 plt.ylabel(r'$\theta$ [$^\circ$]')
 plt.legend()
-# plt.subplot(212)
-# plt.plot(time, velocity_fit, 'b-*', label=r'$\dot{\theta}_{exp}$')
-# plt.plot(sol_ivp.t, sol_ivp.y[1], 'r-*', label=r"$\dot{\theta}_{simu}$")
-# plt.xlabel('Time [s]')
-# plt.ylabel('Velocity [rad/s]')
-# plt.grid()
-# plt.legend()
-# plt.tight_layout()
-# plt.ylim([3, 7])
+plt.ylim([-110, 100])
+plt.subplot(212)
+plt.plot(time_, velocity_, 'k-*', label=r'$\dot{\theta}_{exp}$')
+plt.plot(sol_ivp1.t, sol_ivp1.y[1], 'r-+', label=r"$\dot{\theta}_{simu_1}$")
+plt.plot(sol_ivp1.t[-1] + sol_ivp2.t, sol_ivp2.y[1], 'b-+', label=r"$\dot{\theta}_{simu_2}$")
+plt.plot((sol_ivp1.t[-1] + sol_ivp2.t)[-1] + sol_ivp3.t, sol_ivp3.y[1], 'g-+', label=r"$\dot{\theta}_{simu_3}$")
+plt.xlabel('Time [s]')
+plt.ylabel('Velocity [rad/s]')
+plt.grid()
+plt.legend()
+plt.tight_layout()
+plt.ylim([-2, 7])
 plt.savefig('Sim.pdf')
 plt.show()
