39dde6579ac76cb930e615173cbfcdb56b43653f
Coty Chen <coty@CotydeMacBook-Pro.local>
2023-07-31 23:25:11 +0800

    updated

--- Python-Analysis/pythonProject/Ruigang_Robotic_Arm-Simu_Combination_demostration.py
+++ Python-Analysis/pythonProject/Ruigang_Robotic_Arm-Simu_Combination_demostration.py
@@ -4,7 +4,6 @@ from scipy import signal
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 
-
 inertia = 1 / 3 * (14.7 * 0.001) * ((123.6 * 0.001) ** 2)\
           + (5.5 * 0.001) * ((110 * 0.001) ** 2)  # 0.5 * M * R^2
 omega1 = 5.28
@@ -12,19 +11,19 @@ omega2 = 5.45
 stiffness1 = inertia * (omega1 ** 2)  # I * w^2
 stiffness2 = inertia * (omega2 ** 2)
 slope1 = 0.108
-slope2 = 0.2
+slope2 = 0.4
 torque_friction1 = slope1 * np.pi * stiffness1 / 2 / omega1
 torque_friction2 = slope2 * np.pi * stiffness2 / 2 / omega2
 
-data = pd.read_excel("./20230712_1torque/1_150degrees.xlsx")
-one = 570
-two = 55
+data = pd.read_excel("./20230724/1_clutch_at_25_degrees.xlsx")
+one = 508
+two = 65
 three = 613
 four = 750
 time = np.array(data['Time'].ravel())[one:-5]*0.001
 angle = np.array(data['Degree'].ravel())[one:-5]*np.pi/180
 velocity = np.array(data['Velocity'].ravel())[one:-5]*np.pi/180
-
+print(angle * 180 / np.pi)
 [b, a] = signal.butter(8, 0.05, 'lowpass')  # 配置滤波器 8 表示滤波器的阶数
 velocity_filtered = signal.filtfilt(b, a, velocity)
 
@@ -49,7 +48,7 @@ def eom2(_t, y):
 
 
 def eom3(_t, y):
-    return [y[1], (1.3253182461 * y[1] - 28.8840169731 * y[0])]
+    return [y[1],  - (stiffness2 * y[0] - torque_friction2) / inertia]
 
 
 sol_ivp1 = solve_ivp(eom1, [time1[0], time1[-1]], [angle1[0], velocity1[0]], max_step=0.001)
@@ -61,7 +60,7 @@ plt.subplot(211)
 plt.plot(time, angle * 180 / np.pi, 'b', label='original')
 plt.plot(sol_ivp1.t, sol_ivp1.y[0] * 180 / np.pi, 'r--')
 plt.plot(sol_ivp2.t, sol_ivp2.y[0] * 180 / np.pi, 'r--')
-plt.plot(sol_ivp3.t, sol_ivp3.y[0] * 180 / np.pi, 'r--', label='simulation')
+plt.plot(sol_ivp3.t, sol_ivp3.y[0] * 180 / np.pi, 'r--', label='experiment')
 plt.xlabel('Time [s]')
 plt.ylabel(r'$\theta$ [$^\circ$]')
 plt.ylim([-110, 100])
@@ -71,11 +70,11 @@ plt.plot(time, velocity, 'k-', alpha=0.2, label='original')
 plt.plot(time, velocity_filtered, 'b', label='filtered')
 plt.plot(sol_ivp1.t, sol_ivp1.y[1], 'r--')
 plt.plot(sol_ivp2.t, sol_ivp2.y[1], 'r--')
-plt.plot(sol_ivp3.t, sol_ivp3.y[1], 'r--', label='simulation')
+plt.plot(sol_ivp3.t, sol_ivp3.y[1], 'r--', label='experiment')
 plt.xlabel('Time [s]')
 plt.ylabel('Velocity [rad/s]')
 plt.legend()
 plt.tight_layout()
 plt.ylim([-2, 7])
-plt.savefig('./PDF-File/Simulation.pdf')
+plt.savefig('./PDF-File/Sim.pdf')
 plt.show()
