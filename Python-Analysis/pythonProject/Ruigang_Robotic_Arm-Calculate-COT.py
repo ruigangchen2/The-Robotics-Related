a12564899a901956728deded3a0fbe6e4b3d51c8
Coty Chen <coty@CotydeMacBook-Pro.local>
2023-09-26 09:09:46 +0800

    updated

--- Python-Analysis/pythonProject/Ruigang_Robotic_Arm-Calculate-COT.py
+++ Python-Analysis/pythonProject/Ruigang_Robotic_Arm-Calculate-COT.py
@@ -7,10 +7,10 @@ print("start angle is: %.2f degrees" % (theta_start * 180 / np.pi))
 theta_acc = 10 * np.pi / 180  # The acceleration angle
 print("acceleration angle is: %f degrees" % (theta_acc * 180 / np.pi))
 
-theta_dec =  0 * np.pi / 180  # The deceleration angle
+theta_dec =  20 * np.pi / 180  # The deceleration angle
 print("deceleration angle is: %f degrees" % (theta_dec * 180 / np.pi))
 
-theta_goal = 110 * np.pi / 180
+theta_goal = 130 * np.pi / 180
 print("goal angle is: %.2f degrees" % (theta_goal * 180 / np.pi))
 
 g = 9.8
@@ -20,10 +20,10 @@ J = 1 / 3 * (14.7 * 0.001) * ((123.6 * 0.001) ** 2)\
           + (5.5 * 0.001) * ((110 * 0.001) ** 2)  # 0.5 * M * R^2
 print("rotation inertia is: %f Kg·m²" % J)
 
-w1 = 5.22
+w1 = 5.2
 print("omega1 is: %f rad/s" % w1)
 
-w2 = 9.12
+w2 = 8.0
 print("omega2 is: %f rad/s" % w2)
 
 k1 = J * (w1 ** 2)
@@ -31,6 +31,9 @@ print("stiffness1 is: %f N/m" % k1)
 
 k2 = J * (w2 ** 2)
 print("stiffness2 is: %f N/m" % k2)
+
+total_energy = 0.5 * k1 * (theta_start ** 2)
+print("total_energy is: %f J" % total_energy)
 
 energy_acc = 0.5 * k1 * ((theta_start ** 2) - ((theta_start - theta_acc) ** 2))
 print("energy_acc is: %f J" % energy_acc)
